5ec8a696e9ad83e560fbd50ccdb759c5b1f4d68b
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-11-06 03:49:17 -0500

    added folder

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 const data = [
-    {
-      id: 1,
-      title: 'Sample Product 1',
-      description: 'This is a sample product description.',
-      price: 29.99,
-      colors: ['Red', 'Blue'],
-      features: ['Feature 1', 'Feature 2'],
-      image: 'https://example.com/sample-image.jpg', // Replace with your hosted image link
-    },
-    {
-      id: 2,
-      title: 'Sample Product 2',
-      description: 'Another sample product description.',
-      price: 39.99,
-      colors: ['Green', 'Yellow'],
-      features: ['Feature 3', 'Feature 4'],
-      image: 'https://example.com/another-image.jpg', // Replace with your hosted image link
-    },
-    // Add more data records here
-  ];
-  
-  module.exports = data;
+  {
+    id: 1,
+    title: 'Sample Product 1',
+    description: 'This is a sample product description.',
+    price: 29.99,
+    colors: ['Red', 'Blue'],
+    features: ['Feature 1', 'Feature 2'],
+    image: 'https://example.com/sample-image.jpg', // Replace with your hosted image link
+  },
+  {
+    id: 2,
+    title: 'Sample Product 2',
+    description: 'Another sample product description.',
+    price: 39.99,
+    colors: ['Green', 'Yellow'],
+    features: ['Feature 3', 'Feature 4'],
+    image: 'https://example.com/another-image.jpg', // Replace with your hosted image link
+  },
+  // Add more data records here
+];
+
+module.exports = data;
