be5a8a05176b6159ee09947d8cee404218a53e4c
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-11-06 03:18:02 -0500

    ads

--- public/script.js
+++ public/script.js
@@ -2,21 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('/api/data')
     .then((response) => response.json())
     .then((data) => {
-      const itemContainer = document.getElementById('item-container');
-      data.forEach((item) => {
-        const itemCard = document.createElement('div');
-        itemCard.classList.add('item-card');
-
-        itemCard.innerHTML = `
-          <img src="${item.image}" alt="${item.name}">
-          <h2>${item.name}</h2>
-          <p>${item.description}</p>
-          <p>Price: $${item.price.toFixed(2)}</p>
-          <p>Colors: ${item.colors.join(', ')}</p>
-          <p>Features: ${item.features.join(', ')}</p>
-        `;
-
-        itemContainer.appendChild(itemCard);
+      const productList = document.getElementById('product-list');
+      data.forEach((product) => {
+        // Create and append HTML elements to display the product data
+        // Use data from the product object to populate the elements
       });
     })
     .catch((error) => console.error(error));
--- server.js
+++ server.js
@@ -1,31 +1,10 @@
+// server.js
 const express = require('express');
 const app = express();
-
-// Sample data
-const items = [
-  {
-    id: 1,
-    name: 'Item 1',
-    description: 'This is the first item.',
-    price: 10.99,
-    colors: ['Red', 'Blue'],
-    features: ['Feature A', 'Feature B'],
-    image: 'https://example.com/item1.jpg',
-  },
-  {
-    id: 2,
-    name: 'Item 2',
-    description: 'This is the second item.',
-    price: 15.99,
-    colors: ['Green', 'Yellow'],
-    features: ['Feature X', 'Feature Y'],
-    image: 'https://example.com/item2.jpg',
-  },
-  // Add more items here
-];
+const data = require('./data'); // Import your data
 
 app.get('/api/data', (req, res) => {
-  res.json(items);
+  res.json(data);
 });
 
 const port = process.env.PORT || 3000;
