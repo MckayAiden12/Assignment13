5cf64aab304f731e9f491913509c6487444a2419
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-11-06 03:22:50 -0500

    added new display

--- public/script.js
+++ public/script.js
@@ -3,9 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((response) => response.json())
     .then((data) => {
       const productList = document.getElementById('product-list');
+
       data.forEach((product) => {
-        // Create and append HTML elements to display the product data
-        // Use data from the product object to populate the elements
+        const itemCard = document.createElement('div');
+        itemCard.classList.add('item-card');
+
+        itemCard.innerHTML = `
+          <img src="${product.image}" alt="${product.title}">
+          <h2>${product.title}</h2>
+          <p>${product.description}</p>
+          <p>Price: $${product.price.toFixed(2)}</p>
+          <p>Colors: ${product.colors.join(', ')}</p>
+          <p>Features: ${product.features.join(', ')}</p>
+        `;
+
+        productList.appendChild(itemCard);
       });
     })
     .catch((error) => console.error(error));
