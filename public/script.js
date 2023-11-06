082ac54b973f606c5f9c4f6f186c561400b16d0d
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-11-06 03:08:06 -0500

    made fixes

--- public/script.js
+++ public/script.js
@@ -1,6 +1,23 @@
-fetch('/api/data')
-  .then((response) => response.json())
-  .then((data) => {
-    // Create and append HTML elements to display data beautifully
-  })
-  .catch((error) => console.error(error));
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/api/data')
+    .then((response) => response.json())
+    .then((data) => {
+      const itemContainer = document.getElementById('item-container');
+      data.forEach((item) => {
+        const itemCard = document.createElement('div');
+        itemCard.classList.add('item-card');
+
+        itemCard.innerHTML = `
+          <img src="${item.image}" alt="${item.name}">
+          <h2>${item.name}</h2>
+          <p>${item.description}</p>
+          <p>Price: $${item.price.toFixed(2)}</p>
+          <p>Colors: ${item.colors.join(', ')}</p>
+          <p>Features: ${item.features.join(', ')}</p>
+        `;
+
+        itemContainer.appendChild(itemCard);
+      });
+    })
+    .catch((error) => console.error(error));
+});
